91b1702db7088db8159c85708a6c1780e10b7b60
BrayanFortes <98465059+BrayanFortes@users.noreply.github.com>
2025-06-02 17:07:47 -0300

    Update Exercicio 1.py

--- Exercicio 1.py
+++ Exercicio 1.py
@@ -1,9 +1,10 @@
 import requests
 
+
 API_KEY = "aa7c59dda39ed745c1300fc174d89f25"
-moeda1 = 'BRL'
+moeda1 = str(input("Para qual moeda você vc vai converter? Ex:(BRL)"))
 quantia = float(input("Quanto de Dinheiro você tem? R$"))
-print("|###################|\n 1- Dólar\n 2- Euro\n 3- Ienes\n|###################| ")
+print("|###################|\n 1- Dólar\n 2- Euro\n 3- Ienes\n 4- PesoMexicano \n 5- Escreva para qual será convertida? \n|###################| ")
 opcao = int(input("Qual a Opção:"))
 
 if opcao == 1:
@@ -12,6 +13,11 @@ elif opcao == 2:
     moeda2 = 'EUR'
 elif opcao == 3:
     moeda2 = 'JPY'
+elif opcao == 4:
+    moeda2 = 'BRL'
+elif opcao == 5:
+    moeda2 = str(input("Qual a Moeda que você quer converter? Ex:(USD)"))
+    url = f"https://api.exchangeratesapi.io/v1/convert?access_key={API_KEY}&from={moeda1}&to={moeda2}&amount={quantia}"
 else:
     print("Opção inválida!")
     exit()
