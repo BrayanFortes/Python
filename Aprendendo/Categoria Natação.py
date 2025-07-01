fcab74fea64829608e54aadc2fe075b857306819
BrayanFortes <98465059+BrayanFortes@users.noreply.github.com>
2025-06-30 21:52:01 -0300

    .

--- Aprendendo/Categoria Natação.py
+++ Aprendendo/Categoria Natação.py
@@ -12,9 +12,9 @@ converta = AnoAtual.year - idade.year
 Categorias = ['Mirin I', 'Mirin II', 'Petiz I', 'Petiz II', 'Infantil I', 'Infantil II', 'Juvenil I', 'Juvenil II', 'Júnior I', 'Júnior II', 'Sênior']
 faixa = [9,10,11,12,13,14,15,16,17,18,19]
 for i in range(len(faixa)):
-    if converta == faixa[i] or (i == len(faixa) - 1 and converta >- faixa[i]):
+    if converta == faixa[i] or (i == len(faixa) - 1 and converta >= faixa[i]):
        sub = Categorias[i]
 print(f"""Nome: {nome}
 Idade: {converta}
 Categoria: {sub}""")
-    
+# Este código determina a categoria de natação de uma pessoa com base na sua idade.
